21c4942683022f36ef56af3fc555494302853ae9
AlisaEx <eatliightning@gmail.com>
2013-06-17 13:52:12 -0400

    added ship constructor and loop to add ships

--- game.js
+++ game.js
@@ -1,29 +1,28 @@
 var spaceCanvas = document.getElementById('space');
 var context = spaceCanvas.getContext('2d');
 var federation = [];
-var borgShip = {
-  x: 200, y: 100, width: 200, height: 200,
-  draw: function(context)
-    {context.beginPath();
-    context.rect(borgShip.x, borgShip.y, borgShip.width, borgShip.height);
-    context.fillStyle = 'black';
-    context.fill();}
+function Ship(x, y, width, height){
+  this.x = x; this.y = y; this.width = width; this.height = height;
+}
+Ship.prototype.draw = function(context){
+  context.beginPath();
+  context.rect(this.x, this.y, this.width, this.height);
+  context.fillStyle = 'black';
+  context.fill();
 };
-var federationShip = {
-  x: 600,
-  y: 100,
-  draw: function(context)
-    {context.beginPath();
-    context.rect(this.x, this.y, 10, 10);
-    context.fillStyle = 'black';
-    context.fill();}
-};
+var borgShip = new Ship(100,100,200,200);
 
-
+var shipY = 100;
+for (i=0; i<5; i++){
+  federation[i] = new Ship(600,shipY, 10,10);
+  shipY += 100;
+}
 
 borgShip.draw(context);
-federationShip.draw(context);
 
+federation.forEach(function(ships){
+  ships.draw(context);
+})
 
 spaceCanvas.addEventListener('click', function() {
   context.clearRect(borgShip.x, borgShip.y, borgShip.width, borgShip.height);
