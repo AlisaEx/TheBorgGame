c4e4e430b052fa6636fbdd0e6735983635949733
AlisaEx <eatliightning@gmail.com>
2013-06-26 18:22:18 -0400

    started adding words scrolling for start screen

--- game.js
+++ game.js
@@ -5,6 +5,9 @@ var clickCount = 0;
 var animate = null;
 var federation = [];
 var start = false;
+var textX = 100;
+var textY = spaceCanvas.height;
+
 function collides(a, b) {
   return a.x < b.x + b.width && a.x + a.width > b.x &&
          a.y < b.y + b.height && a.y + a.height > b.y;
@@ -23,10 +26,9 @@ Entity.prototype.draw = function(){
   context.fillStyle = 'black';
   context.closePath();
   context.fill();   
-
 };
-borg = {
-x: 0, y:0, width: 400, height: 400, dx: 0, dy: 0, imageLocation: 'images/borg.png'};
+
+borg = {x: 0, y:0, width: 400, height: 400, dx: 0, dy: 0, imageLocation: 'images/borg.png'};
 
   // loop to create federation ships
 for (i=0; i<5; i++){
@@ -65,37 +67,32 @@ function whichKeyPress(e){
     gameDraw();
   }
 }
-function gameDraw(){
-  if (start === false){
-    startGame();
+
+var text = ["Stardate 58.3582749:",
+    "The starfleet has been attacked by a BORG vessel.",
+    "lkasdfjnasdf", "enter to start"];
+function scrollText(){
+  for (i =  text.length-1; i >= 0; i--) {
+    context.fillStyle = 'white';
+    context.font = '30pt Arial';
+    context.fillText(text[i], textX, textY);
+    textY -= 50;
+    context.clearRect(spaceCanvas.width, 10, textX,textY+50);
   }
-  else{
-    context.clearRect(0,0,spaceCanvas.width, spaceCanvas.height);
-    federation.forEach(function(ships){
-      ships.draw();
-    })
-    drawBorg();
-    spaceCanvas.addEventListener('click', clickMouse, false);
-  }
-};
+  // context.clearRect(spaceCanvas.width, spaceCanvas.height, 0,0);
+  setTimeout(scrollText,100);
+}
 
 function startGame(){
-  context.font = '30pt Ariel';
-  context.fillStyle = 'white';
-  context.fillText("Defeat the BORG",400,300);
-  context.fillText("Click the mouse to attack",350,400);
+  // context.font = '30pt Ariel';
+  // context.fillStyle = 'white';
+  // context.fillText("Defeat the BORG ship",textX,textY);
+  // context.fillText("Press Enter to begin",textX,textY+100);
+  animate = setTimeout(scrollText,100);
   document.onkeypress = whichKeyPress;
 };
 
-function loseGame(){
-  clearInterval(animate);
-  spaceCanvas.removeEventListener('click', clickMouse);
-  context.clearRect(0, 0, spaceCanvas.width, spaceCanvas.height)
-  drawBorg();
-  context.font = '30pt Ariel';
-  context.fillStyle = 'white';
-  context.fillText("You have been assimilated.",300,400);
-};
+
 function attackBorg(){
   for (var i = 0; i < 5; i++) {
     if (collides(federation[i], borgShip)===true || collides(federation[i+5], borgShip)===true){
@@ -115,6 +112,31 @@ movement = function(ship){
   ship.x += (220-ship.x)*ship.speed;
   ship.y += (95-ship.y)*ship.speed;
   ship.draw(context);
+};
+function loseGame(){
+  clearTimeout(animate);
+  spaceCanvas.removeEventListener('click', clickMouse);
+  context.clearRect(0, 0, spaceCanvas.width, spaceCanvas.height)
+  drawBorg();
+  context.font = '30pt Ariel';
+  context.fillStyle = 'white';
+  context.fillText("You have been assimilated.",300,400);
+};
+
+
+function gameDraw(){
+  if (start === false){
+    startGame();
+  }
+  else{
+    clearInterval(animate);
+    context.clearRect(0,0,spaceCanvas.width, spaceCanvas.height);
+    federation.forEach(function(ships){
+      ships.draw();
+    })
+    drawBorg();
+    spaceCanvas.addEventListener('click', clickMouse, false);
+  }
 };
 
   ///GAME///
---
+++ text.js
@@ -0,0 +1,3 @@
+var text = ["Stardate 58.3582749:",
+		"The starfleet has been attacked by a BORG vessel.",
+		"lkasdfjnasdf", "enter to start"];
