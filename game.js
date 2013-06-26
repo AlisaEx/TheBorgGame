5197ec12295c8d5f8da3ccdd1bed6c7fa38cc135
AlisaEx <eatliightning@gmail.com>
2013-06-26 11:48:59 -0400

    press enter to start game

--- game.js
+++ game.js
@@ -4,10 +4,12 @@ var context = spaceCanvas.getContext('2d');
 var clickCount = 0;
 var animate = null;
 var federation = [];
+var start = false;
 function collides(a, b) {
   return a.x < b.x + b.width && a.x + a.width > b.x &&
          a.y < b.y + b.height && a.y + a.height > b.y;
 };
+
 function Entity(x,y,width,height,speed){
   this.x = x;
   this.y = y;
@@ -17,10 +19,10 @@ function Entity(x,y,width,height,speed){
 };
 Entity.prototype.draw = function(){
   context.beginPath();
-    context.rect(this.x, this.y, this.width, this.height);
-    context.fillStyle = 'black';
-    context.closePath();
-    context.fill();   
+  context.rect(this.x, this.y, this.width, this.height);
+  context.fillStyle = 'black';
+  context.closePath();
+  context.fill();   
 
 };
 borg = {
@@ -28,15 +30,15 @@ x: 0, y:0, width: 400, height: 400, dx: 0, dy: 0, imageLocation: 'images/borg.pn
 
   // loop to create federation ships
 for (i=0; i<5; i++){
-  federation[i] = new Entity(600, (i+1)*75, 50, 10, 0.05);
-  federation[i+5] = new Entity((i+1)*75, 600, 10, 50, 0.05);
+  federation[i] = new Entity(600, (i+1)*75, 50, 10, 0.003);
+  federation[i+5] = new Entity((i+1)*75, 600, 10, 50, 0.003);
 }
 
 function clickMouse(){
   if(clickCount===0){
     tractorBeam(context);
     clickCount += 1;
-    animate = setInterval(attackBorg, 500);
+    animate = setInterval(attackBorg, 50);
   }
 }
 function tractorBeam(context){
@@ -56,13 +58,25 @@ drawBorg = function() {
     context.drawImage(borgShip, borg.x, borg.y);
   }
 };
+
+function whichKeyPress(e){
+  if(e.keyCode==13){
+    start = true;
+    gameDraw();
+  }
+}
 function gameDraw(){
-  context.clearRect(0,0,spaceCanvas.width, spaceCanvas.height);
-  federation.forEach(function(ships){
-    ships.draw();
-  })
-  drawBorg();
-  spaceCanvas.addEventListener('click', clickMouse, false);
+  if (start === false){
+    startGame();
+  }
+  else{
+    context.clearRect(0,0,spaceCanvas.width, spaceCanvas.height);
+    federation.forEach(function(ships){
+      ships.draw();
+    })
+    drawBorg();
+    spaceCanvas.addEventListener('click', clickMouse, false);
+  }
 };
 
 function startGame(){
@@ -70,6 +84,7 @@ function startGame(){
   context.fillStyle = 'white';
   context.fillText("Defeat the BORG",400,300);
   context.fillText("Click the mouse to attack",350,400);
+  document.onkeypress = whichKeyPress;
 };
 
 function loseGame(){
@@ -96,13 +111,12 @@ function attackBorg(){
 
   // moves ships towards BORG
 movement = function(ship){
-  context.clearRect(ship.x, ship.y, ship.width, ship.height);
+  context.clearRect(ship.x, ship.y, ship.width+20, ship.height+20);
   ship.x += (220-ship.x)*ship.speed;
   ship.y += (95-ship.y)*ship.speed;
   ship.draw(context);
 };
 
   ///GAME///
-startGame();
-setTimeout(gameDraw,1000);
+gameDraw();
 }())
