969ebc29294b56dbf77b7f944500105d33ba50b0
AlisaEx <eatliightning@gmail.com>
2013-06-27 21:45:16 -0400

    removed useless files

--- collision.js
+++
@@ -1,4 +0,0 @@
-function collides(a, b) {
-  return a.x < b.x + b.width && a.x + a.width > b.x &&
-         a.y < b.y + b.height && a.y + a.height > b.y;
-};
--- text.js
+++
@@ -1,3 +0,0 @@
-var text = ["Stardate 58.3582749:",
-		"The starfleet has been attacked by a BORG vessel.",
-		"lkasdfjnasdf", "enter to start"];
